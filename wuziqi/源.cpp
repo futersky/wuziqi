c6928b410ab747c7f6f5880ec7ce3e8faec2dc68
friendming <qm760681623@163.com>
2019-08-11 13:44:02 +0800

    增加棋盘颜色控制

--- wuziqi/源.cpp
+++ wuziqi/源.cpp
@@ -1,7 +1,8 @@
 #include<graphics.h>
 #include<conio.h>
 
-void initCheckerboard(int stx, int sty, int stepd, int sumx, int sumy);
+void initCheckerboard(int stx, int sty, int stepd, int sumx, int sumy);//棋盘生成函数
+void initCheckerboard(int stx, int sty, int stepd, int sumx, int sumy, int R, int G, int B);//棋盘生成函数（颜色设置）
 
 int main() {
 
@@ -15,6 +16,8 @@ int main() {
 
 void initCheckerboard(int stx, int sty, int stepd, int sumx, int sumy) {
 
+	COLORREF color = getlinecolor();//存储原颜色
+	setlinecolor(COLORREF RGB(100, 100, 100));//设置画线颜色
 	int x = stx;
 	int y = sty;
 	int step = 0;
@@ -37,5 +40,34 @@ void initCheckerboard(int stx, int sty, int stepd, int sumx, int sumy) {
 		line(x, y, x, result_endy);
 		step++;
 	}
+	setlinecolor(color);//恢复画线颜色
+}
 
+void initCheckerboard(int stx, int sty, int stepd, int sumx, int sumy, int R, int G, int B) {
+
+	COLORREF color = getlinecolor();//存储原颜色
+	setlinecolor(COLORREF RGB(R, G, B));//设置画线颜色
+	int x = stx;
+	int y = sty;
+	int step = 0;
+
+	int result_endx;
+	int result_endy;
+
+	result_endx = stx + stepd * (sumx - 1);
+	result_endy = sty + stepd * (sumy - 1);
+
+	for (y = sty; step < sumy; y = y + stepd) {
+		line(x, y, result_endx, y);
+		step++;
+	}
+
+	step = 0;
+	y = sty;
+
+	for (x = stx; step < sumx; x = x + stepd) {
+		line(x, y, x, result_endy);
+		step++;
+	}
+	setlinecolor(color);//恢复画线颜色
 }
